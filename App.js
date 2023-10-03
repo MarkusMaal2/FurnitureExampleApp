d371c44bd0999869dc248868306eac93b26c0adf
Markus Maal <markus.maal@voco.ee>
2023-10-03 09:20:02 +0300

    Google sign-up/sign-in functionality

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import { React, useEffect } from "react";
 import { SafeAreaView } from "react-native";
 import Splash from "./src/screens/auth/Splash";
 import SignUp from "./src/screens/auth/SignUp";
 
-const WEB_CLIENT_ID = "545318985288-elm8jh9d5jkqr0k8ktst5k1lhqv13627.apps.googleusercontent.com";
-const IOS_CLIENT_ID = "545318985288-vq9vhqinvsjoudjf7npesdrn5i7heho0.apps.googleusercontent.com";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import Config from "react-native-config";
 
 const App = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: [
+        'https://www.googleapis.com/auth/drive.readonly'
+      ],
+      webClientId: Config.WEB_CLIENT_ID,
+      offlineAccess: true,
+      iosClientId: Config.IOS_CLIENT_ID,
+    })
+  })
   return (
     <SafeAreaView>
       <SignUp/>
--- src/components/GoogleLogin/index.js
+++ src/components/GoogleLogin/index.js
@@ -1,10 +1,33 @@
 import React from "react";
 import { styles } from "./styles"
 import { TouchableOpacity, Image } from "react-native";
+import { GoogleSignin, statusCodes } from "@react-native-google-signin/google-signin";
 
 const GoogleLogin = () => {
+    const signIn = async() => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            console.log(
+                'userInfo => ', userInfo 
+            )
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                console.log("Sign in cancelled");
+            }
+            else if (error.code === statusCodes.IN_PROGRESS) {
+                console.log("In progress");
+            }
+            else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                console.log("Play Services not available");
+            }
+            else {
+                console.log(error);
+            }
+        }
+    }
     return (
-        <TouchableOpacity activeOpacity={0.6} style={styles.container}>
+        <TouchableOpacity onPress={signIn} activeOpacity={0.6} style={styles.container}>
             <Image style={styles.image} source={require('../../assets/google.png')} />
         </TouchableOpacity>
     )
