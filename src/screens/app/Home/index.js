da6ba10ccef213b5c10312d485e585b26fa2114f
Markus Maal <markus.maal@voco.ee>
2023-10-03 11:26:24 +0300

    Header component and toggle search

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -3,10 +3,13 @@ import {View, Text} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
 
+import Header from "../../../components/Header";
+
 const Home = () => {
     return (
         <SafeAreaView>
             <View style={styles.container}>
+                <Header showSearch={true} title="Find All You Need" />
                 <Text>Home, sweet home!</Text>
             </View>
         </SafeAreaView>
